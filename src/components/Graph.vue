0e0fc9e879fb2335152f5bee098196fd3b97670b
jeanne <jeanne.chaverot@epfl.ch>
2018-10-26 09:24:00 +0200

    Add simple backfard forward links

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -39,18 +39,44 @@ export default {
       })
       block.backlinks.forEach(backlink => {
         // for each backlink of each block
-        const linkTo = this.blocks.find(b => misc.uint8ArrayCompare(backlink, b.hash))
-        if (linkTo) {
-          const indexDiff = (block.index - linkTo.index)
+        const to = this.blocks.find(b => misc.uint8ArrayCompare(backlink, b.hash))
+        if (to) {
+          const indexDiff = (block.index - to.index)
           group.append('line')
             .attr('x1', BLOCK_SIZE / 2)
             .attr('y1', BLOCK_SIZE / 2)
-            .attr('x2', -indexDiff * BLOCK_SEPARATION + BLOCK_SIZE)
+            .attr('x2', - indexDiff * BLOCK_SEPARATION + BLOCK_SIZE)
             .attr('y2', BLOCK_SIZE / 2)
             .attr('stroke-width', 2)
             .attr('stroke', 'green')
         }
       })
+      let maxDiff = 0
+      block.forward.forEach(forward => {
+        const to = this.blocks.find(b => misc.uint8ArrayCompare(forward.to, b.hash))
+        if (to) {
+          const indexDiff = (to.index - block.index)
+          maxDiff = indexDiff > maxDiff ? indexDiff : maxDiff
+          const [x1, y1] = [BLOCK_SIZE / 2, - indexDiff * BLOCK_SIZE]
+          const [x2, y2] = [BLOCK_SIZE / 2 + indexDiff * BLOCK_SEPARATION - indexDiff * BLOCK_SIZE,  - indexDiff * BLOCK_SIZE]
+          const [x3, y3] = [BLOCK_SIZE / 2 + indexDiff * BLOCK_SEPARATION, 0]
+          group.append('polyline')
+            .attr('points', `${x1},${y1} ${x2},${y2} ${x3},${y3}`)
+            .attr('stroke-width', 2)
+            .attr('fill', 'none')
+            .attr('stroke', 'blue')
+        }
+      })
+      if (maxDiff) {
+        group.append('line')
+          .attr('x1', BLOCK_SIZE / 2)
+          .attr('y1', 0)
+          .attr('x2', BLOCK_SIZE / 2)
+          .attr('y2', - maxDiff * BLOCK_SIZE)
+          .attr('stroke-dasharray', '5,5')
+          .attr('stroke-width', 2)
+          .attr('stroke', 'blue')
+      }
     })
   }
 }

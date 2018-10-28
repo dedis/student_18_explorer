1a1e4d933c7f54d5e001d1fb081c63046f65111f
jeanne <jeanne.chaverot@epfl.ch>
2018-10-28 18:08:10 +0100

    Add arrows and visual block separation

--- src/components/Graph.vue
+++ src/components/Graph.vue
@@ -2,6 +2,19 @@
 <v-container >
   <v-card height="500px">
     <svg height="500px" />
+    <defs>
+  <marker
+    id="arrow"
+    markerUnits="strokeWidth"
+    markerWidth="12"
+    markerHeight="12"
+    viewBox="0 0 12 12"
+    refX="6"
+    refY="6"
+    orient="auto">
+    <path d="M2,2 L10,6 L2,10 L6,6 L2,2" style="fill: #f00;"></path>
+  </marker>
+</defs>
 </v-card>
 </v-container>
 </template>
@@ -26,6 +39,21 @@ export default {
       rect.attr('width', BLOCK_SIZE)
         .attr('height', BLOCK_SIZE * block.height)
         .attr('fill', 'brown')
+
+      // blocks visual separation
+      var i = 1
+      while (i < block.height) {
+        group.append('line')
+          .attr('x1', 0)
+          .attr('y1', i * BLOCK_SIZE)
+          .attr('x2', BLOCK_SIZE)
+          .attr('y2', i * BLOCK_SIZE)
+          .attr('stroke-width', 2)
+          .attr('stroke', '#bc5a45')
+          .attr('stroke-dasharray', '4')
+        i = i + 1
+      }
+      // blocks hash and index display
       rect.on('mouseenter', () => {
         svg.append('text')
           .attr('x', 20)
@@ -37,6 +65,8 @@ export default {
       rect.on('mouseleave', () => {
         d3.selectAll('.infoText').remove()
       })
+
+      // backward links display
       block.backlinks.forEach(backlink => {
         // for each backlink of each block
         const to = this.blocks.find(b => misc.uint8ArrayCompare(backlink, b.hash))
@@ -45,26 +75,40 @@ export default {
           group.append('line')
             .attr('x1', BLOCK_SIZE / 2)
             .attr('y1', BLOCK_SIZE / 2)
-            .attr('x2', - indexDiff * BLOCK_SEPARATION + BLOCK_SIZE)
+            .attr('x2', -indexDiff * BLOCK_SEPARATION + BLOCK_SIZE)
             .attr('y2', BLOCK_SIZE / 2)
             .attr('stroke-width', 2)
             .attr('stroke', 'green')
+          const [x1, y1] = [-indexDiff * BLOCK_SEPARATION + BLOCK_SIZE, BLOCK_SIZE / 2]
+          const [x2, y2] = [-indexDiff * BLOCK_SEPARATION + 1.5 * BLOCK_SIZE, BLOCK_SIZE]
+          const [x3, y3] = [-indexDiff * BLOCK_SEPARATION + 1.5 * BLOCK_SIZE, -0.5]
+          group.append('polygon')
+            .attr('points', `${x1},${y1} ${x2},${y2} ${x3},${y3}`)
+            .attr('fill', 'green')
         }
       })
+
+      // forward links display
       let maxDiff = 0
       block.forward.forEach(forward => {
         const to = this.blocks.find(b => misc.uint8ArrayCompare(forward.to, b.hash))
         if (to) {
           const indexDiff = (to.index - block.index)
           maxDiff = indexDiff > maxDiff ? indexDiff : maxDiff
-          const [x1, y1] = [BLOCK_SIZE / 2, - indexDiff * BLOCK_SIZE]
-          const [x2, y2] = [BLOCK_SIZE / 2 + indexDiff * BLOCK_SEPARATION - indexDiff * BLOCK_SIZE,  - indexDiff * BLOCK_SIZE]
-          const [x3, y3] = [BLOCK_SIZE / 2 + indexDiff * BLOCK_SEPARATION, 0]
+          const [x1, y1] = [BLOCK_SIZE / 2, -indexDiff * BLOCK_SIZE]
+          const [x2, y2] = [BLOCK_SIZE / 2 + indexDiff * BLOCK_SEPARATION - indexDiff * BLOCK_SIZE, -indexDiff * BLOCK_SIZE]
+          const [x3, y3] = [indexDiff * BLOCK_SEPARATION, -BLOCK_SIZE / 2]
           group.append('polyline')
             .attr('points', `${x1},${y1} ${x2},${y2} ${x3},${y3}`)
             .attr('stroke-width', 2)
             .attr('fill', 'none')
             .attr('stroke', 'blue')
+          const [x4, y4] = [indexDiff * BLOCK_SEPARATION + BLOCK_SIZE / 2 - BLOCK_SIZE / 2, -BLOCK_SIZE / 2 - BLOCK_SIZE / 2]
+          const [x5, y5] = [indexDiff * BLOCK_SEPARATION + BLOCK_SIZE / 2 - BLOCK_SIZE / 3, -BLOCK_SIZE / 3] // point
+          const [x6, y6] = [indexDiff * BLOCK_SEPARATION - BLOCK_SIZE / 2, -BLOCK_SIZE / 2]
+          group.append('polygon')
+            .attr('points', `${x4},${y4} ${x5},${y5} ${x6},${y6}`)
+            .attr('fill', 'blue')
         }
       })
       if (maxDiff) {
@@ -72,7 +116,7 @@ export default {
           .attr('x1', BLOCK_SIZE / 2)
           .attr('y1', 0)
           .attr('x2', BLOCK_SIZE / 2)
-          .attr('y2', - maxDiff * BLOCK_SIZE)
+          .attr('y2', -maxDiff * BLOCK_SIZE)
           .attr('stroke-dasharray', '5,5')
           .attr('stroke-width', 2)
           .attr('stroke', 'blue')

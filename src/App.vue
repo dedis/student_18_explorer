fa8427722f74b19ed5501c7182b66bb733067d86
jeanne <jeanne.chaverot@epfl.ch>
2018-11-07 14:32:23 +0100

    Create new file for roster and toml configuration

--- src/App.vue
+++ src/App.vue
@@ -67,6 +67,7 @@
 <script>
 import identity, { net, misc } from '@dedis/cothority'
 import Explorer from './Explorer'
+import roster from './default-roster'
 export default {
   name: 'App',
   components: { 'Explorer': Explorer },
@@ -91,23 +92,7 @@ export default {
   },
   /* -- my local roster, to be updated once we'll be dealing with DEDIS' skipchains */
   mounted: function () {
-    const socket = new net.RosterSocket(identity.Roster.fromTOML(`
-      [[servers]]
-        Address = "tls://localhost:7002"
-        Suite = "Ed25519"
-        Public = "57f43d164f82437064a59a02081984974deb821659e03262a67c190f6767e705"
-        Description = "Conode_1"
-      [[servers]]
-        Address = "tls://localhost:7004"
-        Suite = "Ed25519"
-        Public = "40f3a1e76e8bc0bf0f0aad7e963e72c635c0d5e7f9b21e484714d1e8541b22db"
-        Description = "Conode_2"
-      [[servers]]
-        Address = "tls://localhost:7006"
-        Suite = "Ed25519"
-        Public = "b21216372ea04f3c7d25e9386f94d58a564266ff7bd85d7acf79385e076e5f39"
-        Description = "Conode_3"
-    `), 'Skipchain')
+    const socket = new net.RosterSocket(identity.Roster.fromTOML(roster), 'Skipchain')
     /* get all skipchains IDs and map each of them to its hexadecimal form
        we define the first skipchain from the list as the one to be displayed, and the user can switch */
     socket.send('GetAllSkipChainIDs', 'GetAllSkipChainIDsReply', {})
---
+++ src/default-roster.js
@@ -0,0 +1,17 @@
+export default `
+  [[servers]]
+    Address = "tls://localhost:7002"
+    Suite = "Ed25519"
+    Public = "57f43d164f82437064a59a02081984974deb821659e03262a67c190f6767e705"
+    Description = "Conode_1"
+  [[servers]]
+    Address = "tls://localhost:7004"
+    Suite = "Ed25519"
+    Public = "40f3a1e76e8bc0bf0f0aad7e963e72c635c0d5e7f9b21e484714d1e8541b22db"
+    Description = "Conode_2"
+  [[servers]]
+    Address = "tls://localhost:7006"
+    Suite = "Ed25519"
+    Public = "b21216372ea04f3c7d25e9386f94d58a564266ff7bd85d7acf79385e076e5f39"
+    Description = "Conode_3"
+`

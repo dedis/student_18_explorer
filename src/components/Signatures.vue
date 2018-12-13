d28ea53ac216616b044022cd1d736879d159841c
jeanne <jeanne.chaverot@epfl.ch>
2018-12-13 15:02:02 +0100

    create component for signatures

---
+++ src/components/Signatures.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-navigation-drawer width="100%">
+        <v-list>
+          <v-list-group
+            v-for="instruction in instructions"
+            :key="instruction.index"
+          >
+          <v-list-tile slot="activator">
+            <v-list-tile-content>
+              <v-list-tile-title>Signatures ({{instruction.signatures.length}})</v-list-tile-title>
+            </v-list-tile-content>
+          </v-list-tile>
+          <v-list-tile
+            v-if="instruction.signatures.length > 0"
+            v-for="s in instruction.signatures"
+            :key="JSON.stringify(s)"
+            @click=""
+            >
+              <v-card-text>
+                Signature: {{ s.signature}}
+                <br>
+                Signer: {{ s.signer }}
+                <br>
+              </v-card-text>
+            </v-list-tile>
+
+    </v-list-group>
+  </v-list>
+  </v-navigation-drawer>
+</template>
+
+<script>
+export default {
+  props: ['instructions']
+}
+
+
+</script>

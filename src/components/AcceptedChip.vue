fa750b43aa0ab985909dca31d1e11c0092f8644c
jeanne <jeanne.chaverot@epfl.ch>
2018-12-13 17:23:02 +0100

    create component for acceptation of transaction chip

---
+++ src/components/AcceptedChip.vue
@@ -0,0 +1,34 @@
+<template>
+
+<v-card-title primary-title>
+  <p v-if="tx.accepted">
+    <v-chip color="teal" text-color="white">
+      <v-avatar>
+        <v-icon>check_circle</v-icon>
+      </v-avatar>
+      Accepted
+    </v-chip>
+
+    Transaction {{ txi }}
+  </p>
+  <p v-else>
+    <v-chip color="red" text-color="white">
+      <v-avatar>
+        <v-icon>clear</v-icon>
+      </v-avatar>
+      Rejected
+    </v-chip>
+    Transaction {{ txi }}
+  </p>
+</v-card-title>
+
+</template>
+
+<script>
+export default {
+  props: ['tx', 'txi']
+}
+</script>
+
+<style lang="css">
+</style>

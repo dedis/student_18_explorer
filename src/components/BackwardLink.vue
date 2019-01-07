e893c3f4c1c5251aa27de2d617224bc4041114ed
jeanne <jeanne.chaverot@epfl.ch>
2019-01-07 11:17:47 +0100

    Change message for backlink on Genesis blocks

--- src/components/BackwardLink.vue
+++ src/components/BackwardLink.vue
@@ -1,6 +1,11 @@
 <template>
-<div class="">
-  <p v-if="blockIndex === 0">This is a FAKE LINK</p>
+<div v-if="blockIndex===0">
+  <p>Note: The level 0 back link on block 0 is used as a nonce. It does not link to a real block.</p>
+  Level {{hashi}} link: {{ block.slice(0, 15) }}...
+  <v-icon color="green"> arrow_forward </v-icon>
+  {{hash}}
+</div>
+<div v-else>
   Level {{hashi}} link: {{ block.slice(0, 15) }}...
   <v-icon color="green"> arrow_forward </v-icon>
   <a @click="fetchBlock()">{{hash}}</a>

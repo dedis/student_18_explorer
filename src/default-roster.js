3665ca491c82fe78390698d3e02bbb1afe18bb39
Jeff R. Allen <jeff.allen@epfl.ch>
2019-05-15 13:34:38 +0200

    Switch to VPSI leader.

--- src/default-roster.js
+++ src/default-roster.js
@@ -1,14 +1,15 @@
 export default `
 [[servers]]
-  Address = "tls://128.178.151.82:6879"
-  Suite = "Ed25519"
-  Public = "bcb38349b470946c6b3fb3ac66a7436e0a1f138a42af2af3bada0d69a1712c2c"
-  Description = "DEDIS"
-  [servers.Services]
-    [servers.Services.ByzCoin]
-      Public = "857d99faec6e7734b7b0ae7212d1945d0f6174f7fa79fd3c3b051156a816cb8d50ad955f2d788f3c8d6ccf2398f9800c4c70d5effc93e4f4bcd990ed9fe463646db886d98f1bd23f443fa2d446b2f57219a7c9303e4ce3a7747caff81bbafb3e1427978bc1d09ead1cb63ad2fcb8325ad3365a92e1dd80ae20953fe05e5ad842"
-      Suite = "bn256.adapter"
-    [servers.Services.Skipchain]
-      Public = "568e39f8eccf435e67dffe7943e3362ca7bf3fee7024c1457d23ec07e0789b6e742a9a64dfa52b3defbd1760dc24d62890413f0c189867eaef150fa3941ce943246d8ed27981863306c866e59d87bdc53ba5241c3200e32b9980c44f442dccc72881f8a72dd20f9405fa2e8308261e8adc3b8043f3567d3d5b74f18ed48ca521"
-      Suite = "bn256.adapter"
+ Address = "tls://128.178.109.59:6879"
+ Url = "https://conode01.epfl.ch"
+ Suite = "Ed25519"
+ Public = "2e43fb119b319188172c7c1d74a349dba41b357aecf5deec39d75917b956a89e"
+ Description = "VPSI"
+ [servers.Services]
+   [servers.Services.ByzCoin]
+     Public = "6dc2440615ab11d3d256f61f3b02acfa575ac12a4183d4860c7c66d4825574b12fe3e97a7077649ca119a87ef838edb96a7fed48e7e22f880685d86496a78f8942b22bc5b1643bd47f3d2a72376db36a817fe81f40a3edf2405e1590cfdb915f3db784940e95582993ce2ffb293a6bd9470bbb5444ba5fae008c39f3b6b1b091"
+     Suite = "bn256.adapter"
+   [servers.Services.Skipchain]
+     Public = "3275fe9265ecebb41f9202eb4e82311316327a982b3a0ef0c96ad93943f3a0515c616e52bb39fe2fccfef138e727038f529e7b99b6ba2c95455b2b729419956f463e4d1bb4f690aafca15b6d0549787bf8a75cc219f93e614127b089941572671ea62ca0f96254013094aae705b2ca6f588c5bd3357e0bfe73147d91850fe627"
+     Suite = "bn256.adapter"
 `
